e98dd8b9b7d56a49f5acee2bdea5f333b47217a2
Gabriel Queiroz <gabrielqueirozzn@gmail.com>
2019-03-27 22:12:00 -0300

    Adicionar form de input do repository

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as ActionsCreators from "../../store/actions/repositories";
 
-const Home = ({ repositories, addRepositorie }) => {
+const Home = ({ repositories, addRepository }) => {
+  const [repository, setRepository] = useState("");
+
+  const handleChangeRepository = e => {
+    const { value } = e.target;
+    setRepository(value);
+  };
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    alert("submetendo");
+  };
+
   return (
     <div>
-      <h1>react</h1>
+      <h1>Repositories</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          placeholder="add repository"
+          value={repository}
+          onChange={handleChangeRepository}
+        />
+      </form>
       <ul>
         {repositories.map(repo => (
           <li key={repo.id}> {repo.name} </li>
         ))}
       </ul>
-      <button onClick={() => addRepositorie("comer")}>cliquem-me</button>
+      <button onClick={() => addRepository("comer")}>cliquem-me</button>
     </div>
   );
 };
