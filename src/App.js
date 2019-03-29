a9ddb59ebba9f321a7dd677402cb76ccd11dac69
Gabriel Queiroz <gabrielqueirozzn@gmail.com>
2019-03-28 21:43:59 -0300

    Adicionar reactotron no projeto

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 import Home from "./pages/Home";
 import { Provider } from "react-redux";
 import store from "./store";
+import "./config/reactotron";
 
 class App extends Component {
   render() {
+    console.tron.log("aaa");
     return (
       <Provider store={store}>
         <Home />
