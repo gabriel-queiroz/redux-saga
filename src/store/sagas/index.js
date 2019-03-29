b1ef46c16d06ee931ccf91884c93f2363d2db76d
gabriel.queiroz <gabriel.queiroz@phoebus.local>
2019-03-29 13:59:08 -0300

    Ajustar redux-saga para interceptar as actions

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -5,5 +5,5 @@ import addRepository from "./repositories";
 import { Types as RepositoriesTypes } from "../ducks/repositories";
 
 export default function* rootSaga() {
-  yield all([takeLatest(RepositoriesTypes.ADD_REPOSITORY), addRepository]);
+  yield all([takeLatest(RepositoriesTypes.ADD_REPOSITORY, addRepository)]);
 }
